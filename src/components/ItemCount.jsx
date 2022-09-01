dbaa26b609dc4c542312aa5cfa703d3618d6c79f
Martin Zalazar <martin.zqr@gmail.com>
2022-08-31 21:04:47 -0300

    update ItemCount

--- src/components/ItemCount.jsx
+++ src/components/ItemCount.jsx
@@ -1,17 +1,35 @@
 import React from "react";
 import { useState } from "react";
 
-const ItemCount = ({initial,setInitial,stock,onAdd}) => {
-    [initial,setInitial] = useState(initial)
+const ItemCount = ({ initial, setInitial, stock, onAdd }) => {
+  [initial, setInitial] = useState(initial);
   return (
     <div className="container-fluid badge bg-dark text-wrap fs-1">
       <h1>{initial}</h1>
       <div>
-        <button disabled={initial === 0 ? true : false} onClick={()=> setInitial(initial - 1)}> - </button>
-        <button disabled={initial === stock ? true : false} onClick={()=> setInitial(initial + 1)}> + </button>
+        <button
+          disabled={initial === 0 ? true : false}
+          onClick={() => setInitial(initial - 1)}
+        >
+          {" "}
+          -{" "}
+        </button>
+        <button
+          disabled={initial === stock ? true : false}
+          onClick={() => setInitial(initial + 1)}
+        >
+          {" "}
+          +{" "}
+        </button>
       </div>
       <div onClick={onAdd}>
-        <button disabled={initial === 0 || initial === stock ? true : false} onClick={()=> console.log("Items agregados " + initial)}> Add </button>
+        <button
+          disabled={initial === 0 ? true : false}
+          onClick={() => console.log("Items agregados " + initial)}
+        >
+          {" "}
+          Add{" "}
+        </button>
       </div>
     </div>
   );
